73bc20276b1ada9ec4614f45502d2075e0f4c5e7
Maite <maite.v.m27@gmail.com>
2021-10-29 11:36:06 +0200

    Primer commit

---
+++ src/main/java/com/add/conectores/Consultar.java
@@ -0,0 +1,36 @@
+package com.add.conectores;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Consultar {
+
+	public static void main(String[] args) {
+		
+		try {
+			
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ejemplo", "root", "Contrase�a21");
+			Statement st =con.createStatement();
+			
+			ResultSet rs =st.executeQuery("select * from tablaex");
+			
+			while(rs.next()) {
+				
+				System.out.println(rs.getInt(1)+" "+rs.getString(2)+ " " +rs.getString(3)+ " "+rs.getString(4)+ " "+ rs.getDouble(5));
+			}
+			
+			con.close();
+		}
+		
+		catch(SQLException e) {
+			
+			System.out.println(e);
+		}
+		
+
+	}
+
+}
